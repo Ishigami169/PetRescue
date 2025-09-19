a0ecccb48bf479b01c1065d6709d35ce69c0557b
Ishigami169 <139321130+Ishigami169@users.noreply.github.com>
2025-09-19 15:41:14 +0530

    permission fix

--- petrescue/users/permissions.py
+++ petrescue/users/permissions.py
@@ -2,15 +2,10 @@
 
 from rest_framework import permissions
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
+class IsOwnerOrAdmin(permissions.BasePermission):
     """
-    Custom permission to only allow owners of an object to edit it.
+    Custom permission to only allow owners of an object or admins to interact with it.
     """
     def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request, so we'll always
-        # allow GET, HEAD or OPTIONS requests if the user is authenticated.
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        # Write permissions are only allowed to the owner of the object.
-        return obj == request.user
+        # The owner of the object or an admin user can perform any action.
+        return obj == request.user or request.user.is_staff
--- petrescue/users/views.py
+++ petrescue/users/views.py
@@ -1,23 +1,34 @@
 # In users/views.py
 
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import User
 from .serializers import UserSerializer
-from .permissions import IsOwnerOrReadOnly # <-- Import our new permission
+from .permissions import IsOwnerOrAdmin # <-- Import the correct permission
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    @action(detail=False, methods=['get'])
+    def me(self, request):
+        """
+        Return the authenticated user's data.
+        """
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
     def get_permissions(self):
+        """
+        Assigns permissions based on the action.
+        """
         if self.action == 'create':
-            # Anyone can create a new user (register)
             self.permission_classes = [permissions.AllowAny]
         elif self.action == 'list':
-            # Only admin users can see the list of all users
             self.permission_classes = [permissions.IsAdminUser]
-        else:
-            # For retrieve, update, or delete, the user must be authenticated
-            # and can only affect their own account.
-            self.permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+        elif self.action == 'me':
+            self.permission_classes = [permissions.IsAuthenticated]
+        else: # This applies to 'retrieve', 'update', 'partial_update', 'destroy'
+            self.permission_classes = [permissions.IsAuthenticated, IsOwnerOrAdmin]
         return super().get_permissions()
